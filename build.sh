59913ff26e821fac92864a7577e73edf600ece2b
Kevin Barabash <kevinb7@gmail.com>
2014-12-13 10:53:53 -0700

    migrate to tsbuild

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./node_modules/.bin/tsc src/scheduler.ts --outDir lib --target ES5 --declaration --module commonjs --removeComments &&
-mkdir -p dist &&
-./node_modules/.bin/browserify lib/scheduler.js --standalone Scheduler --outfile dist/scheduler.js
--- dist/scheduler.js
+++ dist/scheduler.js
@@ -1,4 +1,4 @@
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Scheduler=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Scheduler=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./lib/scheduler.js":[function(require,module,exports){
 var basic = require("../node_modules/basic-ds/lib/basic");
 var Scheduler = (function () {
     function Scheduler() {
@@ -79,7 +79,7 @@ var Scheduler = (function () {
 })();
 module.exports = Scheduler;
 
-},{"../node_modules/basic-ds/lib/basic":2}],2:[function(require,module,exports){
+},{"../node_modules/basic-ds/lib/basic":"/Users/kevin/live-editor/external/stepper/external/scheduler/node_modules/basic-ds/lib/basic.js"}],"/Users/kevin/live-editor/external/stepper/external/scheduler/node_modules/basic-ds/lib/basic.js":[function(require,module,exports){
 var basic;
 (function (basic) {
     var ListNode = (function () {
@@ -267,5 +267,5 @@ var basic;
 })(basic || (basic = {}));
 module.exports = basic;
 
-},{}]},{},[1])(1)
+},{}]},{},["./lib/scheduler.js"])("./lib/scheduler.js")
 });
